e84b6ee3cfdcaaff86d437d774037c0fcdbf3c15
Marco <marco.hernandez.m@usach.cl>
2020-12-26 14:14:50 -0300

    Added fourier transform function
    *Added plot function

--- Lab3.py
+++ Lab3.py
@@ -6,7 +6,7 @@ import scipy.io
 import scipy.misc
 from scipy import signal
 from scipy.io import wavfile
-from scipy.fftpack import fft2,fftshift
+from scipy.fftpack import fft,fftshift,fftfreq
 from PIL import Image
 
 import numpy as np
@@ -28,13 +28,32 @@ class Signal:
         self.freqDomain = []
     
     def readWavFile(self,filePath):
-        self.path = path
+        self.path = filePath
         self.sourceRate, self.data = wavfile.read(filePath)
         audioArrayLenght = len(self.data)
         self.duration = audioArrayLenght / self.sourceRate
         self.timeDomain = np.linspace(0,self.duration,audioArrayLenght)
+    
+    def plotGraph(self,number, xData, yData, xLabel = None, yLabel = None, title = None):
+        plt.figure(number)
+        plt.plot(xData,yData)
+        plt.title(title)
+        plt.xlabel(xLabel)
+        plt.ylabel(yLabel)
+    def fourierTransform(self):
+        self.fourierData = fft(self.data)
+        dt = 1/float(self.sourceRate)
+        self.freqDomain = fftfreq(len(self.data),dt)
 
 
+
+if __name__ == "__main__":
+    wave = Signal()
+    wave.readWavFile("prueba.wav")
+    wave.plotGraph(1,wave.timeDomain,wave.data,xLabel="tiempo(s)",yLabel="f(t)",title = 'A')
+    
+
+    plt.show()
 
 
 
