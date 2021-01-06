07948584a9fd0ccb5aebf22882ee4d96e3fb9483
Marco Hernandez <marco.hernandez.m@usach.cl>
2021-01-06 19:46:18 -0300

    Commented main block
    * Added result images
    * Added demodulated wav file

--- Lab3.py
+++ Lab3.py
@@ -136,14 +136,16 @@ class Signal:
 
 if __name__ == "__main__":
 
+    # Image lecture
     waveAM = Signal()
-    waveAM.readWavFile("Laboratorio-3-Redes-de-Computadores/prueba.wav")
+    waveAM.readWavFile("prueba.wav")
     waveAM.plotGraph(1,waveAM.timeDomain,waveAM.data,xLabel="tiempo(s)",yLabel="f(t)",title = 'Grafico de la señal en funcion del tiempo')
     waveAM.fourierTransform()
-    waveAM.plotGraph(2,waveAM.freqDomain,np.abs(waveAM.fourierData),xLabel="Frecuancias(Hz)",yLabel="F(w)",title = 'Transformada de Fourier de señal modulada AM')
+    waveAM.plotGraph(2,waveAM.freqDomain,np.abs(waveAM.fourierData),xLabel="Frecuancias(Hz)",yLabel="F(w)",title = 'Transformada de Fourier de señal original')
 
-    f = 20000
+    f = 20000 # AM frequency
 
+    # AM modulation
     waveAM.signalExtract(f)
     AmCarrierData = amCarrier(waveAM.sampleTimeRange,f)
     carrierTransform = fft(AmCarrierData)
@@ -154,18 +156,21 @@ if __name__ == "__main__":
     waveAM.plotGraph(5,waveAM.timeDomain,AmCarrierData,xLabel="tiempo(s)",yLabel="f(t)",title = 'Grafico de la señal portadora en funcion del tiempo')
     waveAM.plotGraph(6,waveAM.freqDomain,np.abs(carrierTransform),xLabel="Frecuencias(Hz)",yLabel="F(w)",title = 'Transformada de Fourier señal portadora')
 
+    # AM demodulation
     waveAM.amDemodulation(f, 4000)
     waveAM.plotGraph(7,waveAM.freqDomain,np.abs(waveAM.fourierData),xLabel="Frecuencias(Hz)",yLabel="F(w)",title = 'Transformada de Fourier de señal demodulada AM')
     plt.xlim([-4000,4000])
 
     waveAM.plotGraph(8,waveAM.timeDomain,waveAM.data.real,xLabel="tiempo(s)",yLabel="f(t)",title = 'Grafico de la señal en funcion del tiempo demodulada')
 
+    # Write new signal
     waveAM.write("Señal_AM.wav")
 
-    fm = 10000
+    fm = 20000 # FM frequency
 
+    # FM modulation
     waveFM = Signal()
-    waveFM.readWavFile("Laboratorio-3-Redes-de-Computadores/prueba.wav")
+    waveFM.readWavFile("prueba.wav")
     waveFM.signalExtract(fm)
     waveFM.fmModulation(fm)
     waveAM.plotGraph(9,waveFM.timeDomain,np.abs(waveFM.data),xLabel="Frecuencias(Hz)",yLabel="F(w)",title = 'Grafico de la señal modulada FM en funcion del tiempo')
